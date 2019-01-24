2a1c5e3ef2443eca11fa163c536e18207aeae4a9
Mainlaptop <orangerobotics@gmail.com>
2019-01-24 17:19:00 -0500

    I just created the basic framework for the robot

---
+++ src/main/java/frc/robot/Hatch.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Hatch 
+{
+    private static final int HATCH_GRABBING_MOTOR_ID = 8;
+    private static final int HATCH_ARTICULATING_MOTOR_ID = 9;
+
+    private WPI_VictorSPX hatchGrabber = new WPI_VictorSPX(HATCH_GRABBING_MOTOR_ID);
+    private WPI_VictorSPX hatchArticulating = new WPI_VictorSPX(HATCH_ARTICULATING_MOTOR_ID);
+
+    private static final Hatch instance = new Hatch();
+
+
+    
+    public static Hatch getInstance()
+    {
+        return instance;
+    }
+}
