80e181877e0ae8c67e4c87946e6de8e549955438
msvandad <35608644+msvandad@users.noreply.github.com>
2019-02-19 16:17:48 -0500

    last vision

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -1,116 +1,159 @@
 package frc.robot;
 
+//Imports
+	//Java imports
 import java.util.ArrayList;
 
+	//OpenCV imports
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.*;
-import frc.robot.DriveTrain;
+
+//WPI imports
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoCamera.WhiteBalance;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.opencv.core.*;
-import org.opencv.core.Core.*;
-import org.opencv.features2d.FeatureDetector;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.*;
-import org.opencv.objdetect.*;
 
-
+//Default contructor
 public class Vision
 {
-	private static final Vision instance = new Vision();
-	private final GRIP grip = GRIP.getInstance();
+	//Decalre cameras
+	private UsbCamera cargoCamera;
+	private UsbCamera hatchCamera;
 
-	private UsbCamera cargoCam, hatchCam;
+	//Construct CameraServer object
+	private CameraServer cameraServer = CameraServer.getInstance();
 	
-	private CvSource outputStream;
-	private CvSink cargoFrameGrabber, hatchFrameGrabber;
+	//Decalre CvSinks
+	private CvSink cargoFrameGrabber;
+	private CvSink hatchFrameGrabber;
+
+	//Decalre CvSource
+	private CvSource outputSteam;
+
+	//Declare frames
+	private Mat originalFrame;
+	private Mat processedFrame;
+
+	//Final values for image resolution 
 	private static final int STANDARD_IMG_WIDTH = 160;
 	private static final int STANDARD_IMG_HEIGHT = 120;
 
-	private Mat originalFrame = new Mat();
-	private Mat processedFrame = new Mat();
-<<<<<<< HEAD
-	private Mat contours = new Mat();
-=======
->>>>>>> 049ace2d06d1f301eb2e250164aab02007a1f3c9
-
+	//Final HSV thresholds for cargo(orange ball)
 	private final Scalar HSV_THRESHOLD_LOWER = new Scalar(0.0, 162.8, 240.7);
 	private final Scalar HSV_THRESHOLD_UPPER = new Scalar(29.5, 224.5, 255.0);
 
-<<<<<<< HEAD
-	
+	//Singelton instance
+	private static final Vision instance = new Vision();
 
-=======
->>>>>>> 049ace2d06d1f301eb2e250164aab02007a1f3c9
-	private CameraServer server = CameraServer.getInstance();
-	//private CameraServer server2 = CameraServer.getInstance();
 
-	private void configureCamera(UsbCamera inputCam, int exposureValue)
-	{
-		inputCam.setResolution(STANDARD_IMG_WIDTH, STANDARD_IMG_HEIGHT);
-		inputCam.setFPS(20);
-		//inputCam.setExposureManual(exposureValue);
-		inputCam.setExposureAuto();
-		inputCam.setBrightness(50);
-		inputCam.setWhiteBalanceManual(WhiteBalance.kFixedIndoor);
-	}
-
+	//Default constructor for the vision class
 	public Vision()
 	{
+		//Initialize each camera with a channel and name, pushes non-processed images
+		cargoCamera = cameraServer.startAutomaticCapture("Cargo Camera",0);
+		hatchCamera = cameraServer.startAutomaticCapture("Hatch Camera",1);
 
-		cargoCam = server.startAutomaticCapture(1);
-		hatchCam = server.startAutomaticCapture(0);
+		//Configure resoltuion, FPS, exposure, brightness and white-balance
+		configureCamera(cargoCamera, false);
+		configureCamera(hatchCamera, false);
 
-		cargoFrameGrabber = server.getVideo(cargoCam);
-		hatchFrameGrabber = server.getVideo(hatchCam);
+		//Initialize frame grabber used to grab individual frames from video stream to be processed later
+		cargoFrameGrabber = cameraServer.getVideo(cargoCamera);
+		hatchFrameGrabber = cameraServer.getVideo(hatchCamera);
 
-		//Exposure value for vision is 0, for regular sight it was 5 but idk what the best value is
-		configureCamera(cargoCam, 5);
-		configureCamera(hatchCam, 5);
-		
-		outputStream = server.putVideo("Processed Video Stream", STANDARD_IMG_WIDTH, STANDARD_IMG_HEIGHT);
+		//Push processed or unprocessed frames
+		outputSteam = cameraServer.putVideo("Processed Video", STANDARD_IMG_WIDTH, STANDARD_IMG_HEIGHT);
 	}
 
-<<<<<<< HEAD
-	public void processing()
+	/*Apply an HSV filter, filters the image based on hue, saturation and value(brightness sort of)
+	*@param: lowerHSVBounds, the minimum values for the filtration
+	*@param: upperHSVBounds, the maximum values for the filtration
+	*
+	*@return: processedFrame, binary image
+	*/
+	public Mat  getHSVFitlteredImage(Scalar lowerHSVBounds, Scalar upperHSVBounds)
 	{
-		
-		Core.inRange(originalFrame, HSV_THRESHOLD_LOWER, HSV_THRESHOLD_UPPER, processedFrame);
-		outputStream.putFrame(processedFrame);
-		Imgproc.findContours(processedFrame, new ArrayList<MatOfPoint>(), new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
-	} 
-=======
-
-	public boolean ballInFrame() {
-
-		//Grab the original frame and run it through the predefined GRIP process
+		//Grab frames from cargo camera to be processed
 		cargoFrameGrabber.grabFrame(originalFrame);
-		grip.process(originalFrame);
 
-		if(!grip.filterContoursOutput().isEmpty())
+		//Covert the BGR image to a HSV image
+		Imgproc.cvtColor(originalFrame, processedFrame, Imgproc.COLOR_BGR2HSV);
+
+		//Apply hsv filter
+		Core.inRange(originalFrame, HSV_THRESHOLD_LOWER, HSV_THRESHOLD_UPPER, processedFrame);
+
+		//Return processed frame
+		return processedFrame;
+	}
+
+	/**Gets a list of contorus from a binary image, stores them in an array list
+	 * 
+	 * @param image, binary frame
+	 * @return contoursList
+	 */
+	public ArrayList<MatOfPoint> findExternalContours(Mat image)
+	{
+		//Empty arraylist of mat points to store contours in 
+		ArrayList <MatOfPoint> contoursList = new ArrayList<MatOfPoint>();
+
+		//Mode and method variables, only find external contours
+		int mode = Imgproc.RETR_EXTERNAL;
+		//Again I don't knwo what this means but Simon did so it should work
+		int method = Imgproc.CHAIN_APPROX_SIMPLE;
+
+		//Simon did it for overlapping contours i don't know what is does!!
+		Mat hierarchy = new Mat();
+
+		return contoursList;
+	}
+
+	public Point findContourCenter(MatOfPoint foundContours)
+	{
+		//Decalre the center point
+		Point centerPoint = new Point();
+		//Simon did it, i don't know what is does but it's here
+		Moments moments = Imgproc.moments(foundContours);
+
+		//Find the x position of the center
+		double xCenter = moments.get_m10()/ moments.get_m00();
+
+		//Find the y position of the center
+		double yCenter = moments.get_m01()/ moments.get_m00();
+
+		//Set the X and Y values of the center
+		centerPoint.x = xCenter;
+		centerPoint.y = yCenter;
+
+		return centerPoint;
+	}
+
+	/**
+	 * 
+	 * @param camera, camera object that will be configured
+	 * @param targetingCamera, boolean indicating wether the camera is going to be used for image processing
+	 */
+	public void configureCamera(UsbCamera camera, boolean targetingCamera)
+	{
+		camera.setResolution(STANDARD_IMG_WIDTH, STANDARD_IMG_HEIGHT);
+		camera.setFPS(15);
+		if(targetingCamera)
 		{
-			return true;
+			camera.setExposureManual(5);
 		}
 		else
 		{
-			return false;
+			camera.setExposureAuto();
 		}
 
-		
-	}
->>>>>>> 049ace2d06d1f301eb2e250164aab02007a1f3c9
+		camera.setBrightness(50);
+		camera.setWhiteBalanceManual(WhiteBalance.kFixedIndoor);
 
-	public static Vision getInstance()
-	{
-		return instance;
 	}
-	
 }
